33908f9fdfea3d2465bf9d2eb4103f87b7b6e493
Joe Cheng <joe.cheng2001@gmail.com>
2025-09-17 12:36:06 +0100

    first optical sound demo

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
+#include "tp/assert.h"
 #include "tp/audio.h"
+#include "tp/image.h"
 #include "tp/io.h"
 #include "tp/log.h"
 #include "tp/math.h"
@@ -14,17 +16,39 @@ int main() {
   tp_audio audio = tp_audio_create(
       &allocator, tp_string_from_string_constant("optical-sound"), 1, 44100);
 
+  tp_image image = tp_image_from_bmp(
+      &allocator, tp_string_from_string_constant("image.bmp"));
+
+  TP_ASSERT(image.rows == 735);
+
   tp_slice_i16 samples;
-  i16 samples_buf[44100] = {0};
+  i16 samples_buf[735] = {0};
   samples.data = samples_buf;
   samples.count = sizeof(samples_buf) / sizeof(samples_buf[0]);
   for (usize ii = 0; ii < samples.count; ii++) {
-    samples.data[ii] = tp_math_sin_f32(ii / (2 * 3.14159265)) * 16000;
-  }
-  while (true) {
-    for (usize ii = 0; ii < 44100; ii++) {
-      tp_audio_write(&allocator, audio, samples);
+
+    usize total_luminance = 0;
+    for (usize col = 0; col < image.cols; col++) {
+      switch (image.format) {
+      case TP_PIXEL_FORMAT_GREY: {
+        total_luminance += image.data.grey.data[ii * image.cols + col];
+        break;
+      }
+      case TP_PIXEL_FORMAT_RGB: {
+        rgb px = image.data.rgb.data[ii * image.cols + col];
+        total_luminance += (px.r + px.g + px.b) / 3;
+        break;
+      }
+      }
     }
+
+    f32 average_luminance = (f32)total_luminance / image.cols;
+    i16 sample = ((average_luminance / 255.0) - 0.5) * 2.0 * TP_I16_MAX;
+    samples_buf[ii] = sample;
+  }
+
+  while (true) {
+    tp_audio_write(&allocator, audio, samples);
   }
 
   return 0;
